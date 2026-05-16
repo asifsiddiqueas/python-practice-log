e137c9eabb5c1d0f617b1a6f8b49a915d062d674
asifsiddiqueas <mdasifsiddiqueas@gmail.com>
2026-05-17 02:20:23 +0530

    feat: add odd even number checker

---
+++ 01_basics/012_odd_even.py
@@ -0,0 +1,8 @@
+# Program to check if a number is odd or even.
+
+number = float(input("Enter your Number: "))
+
+if number %2 == 0:
+    print("This is an even Number: ")
+else:
+    print("This is an odd number: ")
