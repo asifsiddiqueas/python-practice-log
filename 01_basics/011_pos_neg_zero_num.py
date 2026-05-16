bdbce96f961830d7bf73dc8d96eb63120449f170
asifsiddiqueas <mdasifsiddiqueas@gmail.com>
2026-05-17 02:19:27 +0530

    feat: add positive, negative and zero number checker

---
+++ 01_basics/011_pos_neg_zero_num.py
@@ -0,0 +1,11 @@
+# Program to check if a number is positive, negative or zero.
+
+# User defined input
+number = float(input("Enter your Number: "))
+
+if number > 0:
+    print("This is a positive number: ")
+elif number == 0:
+    print("This is zero: ")
+else:
+    print("This is a negative number: ")
