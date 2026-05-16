92c6b4945ced9057c4f14974d14cd685fa2a4591
asifsiddiqueas <mdasifsiddiqueas@gmail.com>
2026-05-16 18:35:16 +0530

    feat: generate random number using python random module

---
+++ 01_basics/005_random_number.py
@@ -0,0 +1,11 @@
+#Prgram to generate random number.
+
+#import necessary libraries
+import random
+from random import randint
+
+#Defining the variable for random function
+guess = random.randint
+
+#Display the randomly generated number
+print(f"The randomly generated number is: {guess(0,200)}")
