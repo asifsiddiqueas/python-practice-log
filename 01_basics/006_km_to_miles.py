29ed9cf54d99a11d6c4a8827c41199f5ecb11b6f
asifsiddiqueas <mdasifsiddiqueas@gmail.com>
2026-05-16 18:45:04 +0530

    feat: add km to mile conversion program

---
+++ 01_basics/006_km_to_miles.py
@@ -0,0 +1,7 @@
+#Program to convert kilometers to miles.
+
+#user defined value in km
+kilometer = float(input("Enter the value in km: "))
+
+#Displays the value in Miles with context
+print(f"There are {kilometer*0.621371192} miles in {kilometer} km")
