c541d7a8c11f159b399a034e2ef62fcef9fc5a5e
asifsiddiqueas <mdasifsiddiqueas@gmail.com>
2026-05-28 22:14:08 +0530

    feat: add hcf calculator

---
+++ 01_basics/023_hcf_calculator.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+while num2 != 0:
+    temp = num2
+    num2 = num1 % num2
+    num1 = temp
+
+print("HCF is:", num1)
